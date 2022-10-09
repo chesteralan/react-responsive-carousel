290c7b9a4893fcefe5d39bc570209c44d4fdf8a4
Chester Alan <chesteralantagudin@gmail.com>
2022-10-10 03:08:50 +0800

    make all props optional

--- src/components/Carousel/types.ts
+++ src/components/Carousel/types.ts
@@ -21,56 +21,56 @@ export type StopSwipingHandler = (props: CarouselProps, state: CarouselState) =>
 
 export interface CarouselProps {
     ariaLabel?: string | undefined;
-    axis: 'horizontal' | 'vertical';
+    axis?: 'horizontal' | 'vertical';
     autoFocus?: boolean;
     autoPlay?: boolean;
     centerMode?: boolean;
-    centerSlidePercentage: number;
+    centerSlidePercentage?: number;
     children?: React.ReactChild[];
     className?: string;
     dynamicHeight?: boolean;
     emulateTouch?: boolean;
     infiniteLoop?: boolean;
-    interval: number;
-    labels: {
+    interval?: number;
+    labels?: {
         leftArrow: string;
         rightArrow: string;
         item: string;
     };
-    onClickItem: (index: number, item: React.ReactNode) => void;
-    onClickThumb: (index: number, item: React.ReactNode) => void;
-    onChange: (index: number, item: React.ReactNode) => void;
-    onSwipeStart: (event: React.TouchEvent) => void;
-    onSwipeEnd: (event: React.TouchEvent) => void;
-    onSwipeMove: (event: React.TouchEvent) => boolean;
-    preventMovementUntilSwipeScrollTolerance: boolean;
-    renderArrowPrev: (clickHandler: () => void, hasPrev: boolean, label: string) => React.ReactNode;
-    renderArrowNext: (clickHandler: () => void, hasNext: boolean, label: string) => React.ReactNode;
-    renderIndicator: (
+    onClickItem?: (index: number, item: React.ReactNode) => void;
+    onClickThumb?: (index: number, item: React.ReactNode) => void;
+    onChange?: (index: number, item: React.ReactNode) => void;
+    onSwipeStart?: (event: React.TouchEvent) => void;
+    onSwipeEnd?: (event: React.TouchEvent) => void;
+    onSwipeMove?: (event: React.TouchEvent) => boolean;
+    preventMovementUntilSwipeScrollTolerance?: boolean;
+    renderArrowPrev?: (clickHandler: () => void, hasPrev: boolean, label: string) => React.ReactNode;
+    renderArrowNext?: (clickHandler: () => void, hasNext: boolean, label: string) => React.ReactNode;
+    renderIndicator?: (
         clickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
         isSelected: boolean,
         index: number,
         label: string
     ) => React.ReactNode;
-    renderItem: (item: React.ReactNode, options?: { isSelected: boolean; isPrevious: boolean }) => React.ReactNode;
-    renderThumbs: (children: React.ReactChild[]) => React.ReactChild[];
-    selectedItem: number;
-    showArrows: boolean;
-    showStatus: boolean;
-    showIndicators: boolean;
-    showThumbs: boolean;
-    statusFormatter: (currentItem: number, total: number) => string;
-    stopOnHover: boolean;
-    swipeable: boolean;
-    swipeScrollTolerance: number;
+    renderItem?: (item: React.ReactNode, options?: { isSelected: boolean; isPrevious: boolean }) => React.ReactNode;
+    renderThumbs?: (children: React.ReactChild[]) => React.ReactChild[];
+    selectedItem?: number;
+    showArrows?: boolean;
+    showStatus?: boolean;
+    showIndicators?: boolean;
+    showThumbs?: boolean;
+    statusFormatter?: (currentItem: number, total: number) => string;
+    stopOnHover?: boolean;
+    swipeable?: boolean;
+    swipeScrollTolerance?: number;
     thumbWidth?: number;
-    transitionTime: number;
+    transitionTime?: number;
     useKeyboardArrows?: boolean;
-    verticalSwipe: 'natural' | 'standard';
-    width: number | string;
-    animationHandler: 'slide' | 'fade' | AnimationHandler;
-    swipeAnimationHandler: SwipeAnimationHandler;
-    stopSwipingHandler: StopSwipingHandler;
+    verticalSwipe?: 'natural' | 'standard';
+    width?: number | string;
+    animationHandler?: 'slide' | 'fade' | AnimationHandler;
+    swipeAnimationHandler?: SwipeAnimationHandler;
+    stopSwipingHandler?: StopSwipingHandler;
 }
 
 export interface CarouselState {
